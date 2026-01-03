60d121b6578c17ebc4c95578d1c51f79d62a9e69
Jerrody <huntermlg123465@gmail.com>
2026-01-03 13:26:45 +0200

    Some cleanups.

--- src/engine.rs
+++ src/engine.rs
@@ -6,7 +6,7 @@ use std::sync::Arc;
 
 use bevy_ecs::{
     schedule::{Schedule, ScheduleLabel},
-    world::{self, World},
+    world::World,
 };
 use vma::{Alloc, AllocationOptions, Allocator, AllocatorOptions, MemoryUsage};
 use vulkanalia::{Version, vk::*};
