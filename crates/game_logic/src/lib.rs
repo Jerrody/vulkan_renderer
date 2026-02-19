4978753f04961bd9dc8aa70792d2dd485b7a5801
Jerrody <huntermlg123465@gmail.com>
2026-02-19 10:57:50 +0200

    Adding player jump!

--- crates/game_logic/src/lib.rs
+++ crates/game_logic/src/lib.rs
@@ -9,7 +9,7 @@ use engine::{
     GamePlugin,
     engine::{Camera, ClippingPlanes, Input, LoadModelEvent, Time, Transform},
 };
-use glam::Vec3;
+use glam::{FloatExt, Vec3};
 use winit::keyboard::KeyCode;
 
 #[unsafe(no_mangle)]
@@ -25,7 +25,7 @@ impl GamePlugin for Game {
     }
 
     fn add_systems_update(&self, schedule: &mut bevy_ecs::schedule::Schedule) {
-        schedule.add_systems((move_player, rotate_player));
+        schedule.add_systems((move_player, rotate_player, jump_player));
     }
 }
 
@@ -35,6 +35,16 @@ pub struct PlayerStats {
     pub move_speed: f32,
     pub run_speed: f32,
     pub rotation_speed: f32,
+}
+
+#[derive(Default, Component)]
+pub struct PlayerJump {
+    pub jump_duration: f32,
+    pub jump_height: f32,
+    pub current_duration: f32,
+    pub initial_y_height: f32,
+    pub is_jumping: bool,
+    pub is_falling: bool,
 }
 
 #[derive(Component)]
@@ -72,18 +82,24 @@ fn spawn_entity(mut commands: Commands) {
         rotation_speed: 5.0,
     };
 
+    let player_jump = PlayerJump {
+        jump_duration: 1.5,
+        jump_height: 4.0,
+        ..Default::default()
+    };
+
     let mut player_entity = commands.spawn_empty();
-    player_entity.insert((camera_component, player_stats_component));
+    player_entity.insert((camera_component, player_stats_component, player_jump));
 }
 
 fn move_player(
-    mut player_query: Query<(&mut Transform, &PlayerStats)>,
+    mut player_query: Query<(&mut Transform, &PlayerStats, &PlayerJump)>,
     time: Res<Time>,
     input: Res<Input>,
 ) {
     let delta_time = time.get_delta_time();
 
-    let (mut transform, player_stats) = player_query.single_mut().unwrap();
+    let (mut transform, player_stats, player_jump) = player_query.single_mut().unwrap();
 
     let target_speed = if input.pressed(KeyCode::ShiftLeft) {
         player_stats.run_speed
@@ -128,4 +144,63 @@ fn rotate_player(
     angles.x = angles.x.clamp(-90.0, 90.0);
 
     transform.set_local_euler_angles(angles);
+}
+
+fn jump_player(
+    mut player_query: Query<(&mut Transform, &mut PlayerJump)>,
+    time: Res<Time>,
+    input: Res<Input>,
+) {
+    let delta_time = time.get_delta_time();
+
+    let (mut transform, mut player_jump) = player_query.single_mut().unwrap();
+
+    if player_jump.is_jumping {
+        player_jump.current_duration += delta_time;
+
+        let in_start = if player_jump.is_falling {
+            player_jump.jump_duration / 2.0
+        } else {
+            Default::default()
+        };
+
+        let in_end = if player_jump.is_falling {
+            player_jump.jump_duration
+        } else {
+            player_jump.jump_duration / 2.0
+        };
+
+        let jump_height = player_jump.initial_y_height + player_jump.jump_height;
+        let out_start = if player_jump.is_falling {
+            jump_height
+        } else {
+            player_jump.initial_y_height
+        };
+
+        let out_end = if player_jump.is_falling {
+            player_jump.initial_y_height
+        } else {
+            jump_height
+        };
+
+        let new_height = player_jump
+            .current_duration
+            .remap(in_start, in_end, out_start, out_end);
+        transform.local_position.y = new_height;
+        player_jump.is_falling = player_jump.current_duration > player_jump.jump_duration / 2.0;
+
+        if player_jump.is_falling {
+            if f32::abs(transform.local_position.y - player_jump.initial_y_height) < 0.1 {
+                player_jump.is_jumping = false;
+                player_jump.is_falling = false;
+            }
+        }
+    } else {
+        if input.just_pressed(KeyCode::Space) {
+            player_jump.is_jumping = true;
+            player_jump.is_falling = false;
+            player_jump.current_duration = Default::default();
+            player_jump.initial_y_height = transform.local_position.y;
+        }
+    }
 }
